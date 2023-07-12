8212c7a68b8f2ee791de666f8c568ae997c98260
Serhan ÇINAR <serhan6200@gmail.com>
2023-07-12 15:44:53 +0300

    Handling unexpected errors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
 const app = require("./app");
+const logger = require("./configs/logger");
 
 // ENV Variables
 const PORT = process.env.PORT;
+let server;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+server = app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
 });
+
+// Handle Server Errors
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
